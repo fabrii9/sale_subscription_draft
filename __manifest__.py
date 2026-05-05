26ab12148999658c48b48019cce3dd8ebb380945
Fabrizio Dominguez <78427801+fabrii9@users.noreply.github.com>
2026-05-04 22:43:53 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,10 +5,11 @@
     'summary': 'Generates subscription invoices in draft state instead of validating them automatically',
     'author': 'Fabrizio',
     'license': 'LGPL-3',
-    'depends': ['sale_subscription'],
-    'data': [
-        'views/sale_order_views.xml',
+    'depends': [
+        'sale_subscription',
     ],
+    'data': [],
     'auto_install': False,
     'installable': True,
+    'application': False,
 }
